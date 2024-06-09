73aefe1f8811160ebb36f1f36caea4d2d207df55
Deepanshu Bhargav <deepanshubhargav739@gmail.com>
2024-06-09 18:08:37 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function cursonEffect() {
+    var page1Content = document.querySelector("#page1-content")
+    var cursor = document.querySelector("#cursor")
+
+    page1Content.addEventListener("mouseover", function (dets) {
+        gsap.to(cursor, {
+            x: dets.x,
+            y: dets.y
+        })
+    })
+
+    page1Content.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1
+        })
+    })
+    page1Content.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0
+        })
+    })
+}
+
+cursonEffect();
